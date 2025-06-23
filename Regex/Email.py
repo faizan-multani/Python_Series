35df5b5f35be186362b6cb894e97671b95638ebc
faizan-multani <faizan.multani60@gmail.com>
2025-06-23 20:49:38 +0530

    changes

--- Regex/Email.py
+++ Regex/Email.py
@@ -7,6 +7,8 @@ corey-321-schafer@my-work.net
 '''
 
 pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+                    # (OR)
+# pattern = re.compile(r'[\w.+-]+@[\w-]+[\w.-]+')
 
 matches = pattern.finditer(emails)
 
