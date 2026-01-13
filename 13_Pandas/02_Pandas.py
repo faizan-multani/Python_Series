f7d68610410df857cdaba7e0067a5f03f36caff6
faizan-multani <faizan.multani60@gmail.com>
2026-01-13 08:59:38 +0530

    updates

--- 13_Pandas/02_Pandas.py
+++ 13_Pandas/02_Pandas.py
@@ -1,4 +1,13 @@
 import pandas as pd
+# Read a file
 url = "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv"
 df = pd.read_csv(url)
 print(df)
+
+# how many columns should display -> usecols=[]
+df = pd.read_csv(url,usecols=['petal_width','species'])
+print(df)
+
+# to Write a file
+test_file=df.to_csv('test.csv')
+print(test_file)
